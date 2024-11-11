4e6eb657789709c41a0d300325d4dd8ee48b47c7
SeungWoo Ryu <swstar21c@gmail.com>
2024-11-11 23:57:59 +0900

    #52 - 모달 form에 csrf 토큰 적용
    - 강의 코드에서는 csrf 토큰을 자동으로 적용하기 위해서 `th:action="@{#}"`와 같은 코드를 사용했다.
      - 그러나 위 코드는 이 프로젝트의 스프링 부트 버전과 맞지 않아 정상적으로 동작하지 않는다.
    - 모달을 정상적으로 띄우고 삭제 기능이 동작하도록 하기 위해서 스프링 부트 버전에 맞게 수정했다.
      - `@EnableWebSecurity` 어노테이션을 사용해 `CSRFRequestDataValueProcessor`가 적용될 수 있도록 했다.
      -  csrf 토큰을 담고 있을 input 태그를 추가했다.

--- src/main/java/com/fastcampus/projectboardadmin/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboardadmin/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -24,6 +25,7 @@ import java.util.UUID;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
     @Bean
